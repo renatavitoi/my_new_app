642fe271f709a207ab4cfdf4bc3dd9d3fdc8ced7
renata_band@hotmail.com <renata_band@hotmail.com>
2019-05-07 17:51:07 +0200

    3.8 SimplePagesController
    About and Contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'simple_pages/index'
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#index'
+  root 'simple_pages#about'
+  root 'simple_pages#contact'
 end
