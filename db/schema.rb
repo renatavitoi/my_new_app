529e191ef94859de8ffe6789f8e94600dbf02e9b
renatavitoi <renata_band@hotmail.com>
2019-06-30 10:14:20 +0200

    move back controllers folder, fixed color, add price migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_055925) do
+ActiveRecord::Schema.define(version: 2019_06_30_074808) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_06_09_055925) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
+    t.decimal "price"
   end
 
 end
