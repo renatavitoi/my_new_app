eee7374f3103ae9e15d3f2b04ca055cfa705ad4a
renatavitoi <renata_band@hotmail.com>
2019-07-05 13:38:55 +0200

    fixed adding custom controller actions for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
 end
