72b662c16f335313b40edbd841c5db4dab4f114c
renatavitoi <renata_band@hotmail.com>
2019-07-14 15:02:26 +0200

    5.1 Routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#landing_page'
+  root 'simple_pages#index'
 end
