189450cf95183351452398142ad95e29c0c87054
renatavitoi <renata_band@hotmail.com>
2019-08-04 17:27:39 +0200

    5.3 sending emails

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,18 +3,15 @@
    def index
    end
 
+   def thank_you
+     @name = params[:name]
+     @email = params[:email]
+     @message = params[:message]
+     UserMailer.contact_form(@email, @name, @message).deliver_now
+   end
+
 
    def landing_page
      @products = Product.limit(3)
   end
-end
-
-def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(from: @email,
-      to: 'santovaralstore@gmail.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
 end
