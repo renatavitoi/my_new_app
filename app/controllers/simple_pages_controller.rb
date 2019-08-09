b8d488734e942f01162273a688e141e3e328c3df
renatavitoi <renata_band@hotmail.com>
2019-08-09 15:33:24 +0200

    5.2 fixed forms

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,12 +4,11 @@
    end
 
    def thank_you
-     @name = params[:name]
-     @email = params[:email]
-     @message = params[:message]
-     UserMailer.contact_form(@email, @name, @message).deliver_now
-   end
-
+   @name = params[:name]
+   @email = params[:email]
+   @message = params[:message]
+   UserMailer.contact_form(@email, @name, @message).deliver_now
+ end
 
    def landing_page
      @products = Product.limit(3)
