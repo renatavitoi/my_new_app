4dba5cab206f3db1477904a04b09e940a1a29c43
renatavitoi <renata_band@hotmail.com>
2019-08-10 19:22:02 +0200

    5.3 fixed simple_pages controller.rb

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,13 +1,14 @@
  class SimplePagesController < ApplicationController
 
    def index
+     @products = Product.all
    end
 
    def thank_you
-   @name = params[:name]
-   @email = params[:email]
-   @message = params[:message]
-   UserMailer.contact_form(@email, @name, @message).deliver_now
+     @name = params[:name]
+     @email = params[:email]
+     @message = params[:message]
+     UserMailer.contact_form( @name, @email, @message).deliver_now
  end
 
    def landing_page
