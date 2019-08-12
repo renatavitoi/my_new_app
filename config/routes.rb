7903685496e7409e37e72481ef8eb587a66a50c3
renatavitoi <renata_band@hotmail.com>
2019-08-12 19:07:50 +0200

    5.4 authentication

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/thank_you'
+  get 'simple_pages/login'
   get '/products/:users/1', to: 'products#show'
   get '/products', to: 'products#index'
   post 'simple_pages/thank_you'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#landing_page'
