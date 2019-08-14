1822a5785c73ae2a75f05516755ffda5cd216b8c
renatavitoi <renata_band@hotmail.com>
2019-08-14 18:47:58 +0200

    5.3 fixed user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,9 @@
-  class User < ActiveRecord::Base
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :trackable, :validatable
-    has_many :orders
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :orders
+  has_many :comments
 
-    #give perms to write on login params
-    attr_acessor :login
-
-    # validates username without being case case_sensitive
-    validates :username, presence: :true, uniqueness: {case_sentive: false}
-    # only allow, letter, number, underscore and punctuation.
-
-    # send email after sign up
-    after_create :send_email_signup
-
-    def send_email_signup
-      UserMailer.register_info(self).deliver_now
-end
-
-def login
-  #accepts 'login' as username or email.
-  # whatever it gets as input
-  @login || self.username || self.send_email_signup
 end
