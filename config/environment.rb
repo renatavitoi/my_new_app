5350ca89166ccd150004e1e66db3e5e5e8451e8e
renatavitoi <renata_band@hotmail.com>
2019-08-14 20:38:59 +0200

    5.3 fixed environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -13,5 +13,6 @@ Rails.application.initialize!
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
   domain: 'heroku.com',
-  enable_starttls_auto: true
+  enable_starttls_auto: true,
+  openssl_verify_mode: "none" 
 }
