cf16a0dd86ebe7356100b32a06eed83c2fa5581f
renatavitoi <renata_band@hotmail.com>
2019-08-17 17:47:19 +0200

    5.4 fixed authentication
    5.4  fixed authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :orders, only: [:index, :show, :create, :destroy]
+  devise_for :users, controllers: { registrations: "user_registrations" }
+
+  resources :products do
+    resources :comments
+  end
+
+  resources :users
 
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/thank_you'
-  get 'simple_pages/login'
   get '/products/:users/1', to: 'products#show'
   get '/products', to: 'products#index'
   post 'simple_pages/thank_you'
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#landing_page'
+    root 'simple_pages#landing_page'
+
+
 end
